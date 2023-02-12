2cde290725fc1ae7175658168e82268bbe41a23e
Gustavo Terence <gustavosantosterence@gmail.com>
2023-02-12 14:14:04 -0300

    projeto finalizado

---
+++ src/main/java/dio/digitalInovationonespringboot/service/ClienteService.java
@@ -0,0 +1,17 @@
+package dio.digitalInovationonespringboot.service;
+
+import dio.digitalInovationonespringboot.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
